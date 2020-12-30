f84bd6e0b7493e2314592576357e3e369977a8f7
Ebenezer Scrooge <1641367382@qq.com>
2020-12-30 17:58:07 +0800

    Redesign Model Structure

--- DLDemo.py
+++ DLDemo.py
@@ -4,10 +4,41 @@ from tensorflow.keras.layers import Dense, Input
 
 
 def build_model():
-    input_layer = Input(shape=(4,))
+    input_dims = 2
+    # hp & frags: 2 params
+    input_dims += 1
+    # ranged enemies count: 1 param
+    input_dims += 1
+    # ranged enemies ave distance/range radius: 1 param
+    input_dims += 1
+    # nearest enemies health/full health: 1 param
+    input_dims += 3
+    # nearest enemies direction: 3 params
+    input_dims += 1
+    # second nearest enemies health/full health: 1 param
+    input_dims += 3
+    # second nearest enemies direction: 3 params
+    input_dims += 1
+    # third nearest enemies health/full health: 1 param
+    input_dims += 3
+    # third nearest enemies direction: 3 params
+    input_layer = Input(shape=(input_dims,))
     D1 = Dense(32, activation='relu')
     D2 = Dense(32, activation='relu')
-    O = Dense(4, activation='relu')
+    output_dims = 1
+    # move or not: 1 param
+    output_dims += 3
+    # move to (direction): 3 params
+    output_dims += 1
+    # shoot or not: 1 param
+    output_dims += 3
+    # face to (direction): 3 params
+    output_dims += 1
+    # jump or not: 1 param
+    output_dims += 1
+    # reload or not: 1 param
+
+    O = Dense(output_dims, activation='relu')
     x = D1(input_layer)
     x = D2(x)
     x = O(x)
