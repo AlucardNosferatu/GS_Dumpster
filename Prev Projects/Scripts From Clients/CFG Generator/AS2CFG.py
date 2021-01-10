fd9d7f3848b8cddd4664df78b0641f7466a5f787
Ebenezer Scrooge <1641367382@qq.com>
2021-01-10 22:20:48 +0800

    Update AS2CFG.py

--- Prev Projects/Scripts From Clients/CFG Generator/AS2CFG.py
+++ Prev Projects/Scripts From Clients/CFG Generator/AS2CFG.py
@@ -3,13 +3,8 @@
 lines=[]
 with open("UREnhanced.as", "r") as asp:  # 打开文件
     id=asp.name.replace('.as','')
-    # header = 'asp_upload_name ' + '"' + id + '"' + '\n'
-    header = 'say ' + '"' + id + '"' + '\n'
-    lines.append(header)
-    for i in range(16):
-        lines.append('wait\n')
-    # send_start = 'asp_upload_start ' + '"' + id + '"' + '\n'
-    send_start = 'say ' + '"' + id + '"' + '\n'
+    send_start = 'dick ' + '"upload_start ' + id + '"' + '\n'
+    # send_start = 'say ' + '"' + id + '"' + '\n'
     lines.append(send_start)
     for i in range(16):
         lines.append('wait\n')
@@ -18,13 +13,13 @@ with open("UREnhanced.as", "r") as asp:  # 打开文件
         if not code_sentence:
             break
         else:
-            # send_content = 'asp_upload_content ' + id + ' ' + '"' + code_sentence.replace('\n', '').replace('"', '$QUOTE$') + '"' + '\n'
-            send_content = 'say ' + '"' + code_sentence.replace('\n', '').replace('"', '$QUOTE$') + '"' + '\n'
+            send_content = 'dick ' + '"upload_send_line ' + id + ' ' + code_sentence.replace('\n', '').replace('"', '$QUOTE$') + '"' + '\n'
+            # send_content = 'say ' + '"' + code_sentence.replace('\n', '').replace('"', '$QUOTE$') + '"' + '\n'
             lines.append(send_content)
             for i in range(16):
                 lines.append('wait\n')
     # send_end = 'asp_upload_end ' + '"' + id + '"'
-    send_end = 'say ' + '"' + id + '"'
+    send_end = 'dick ' + '"upload_stop ' + id + '"'
     lines.append(send_end)
 with open("upload_asp.cfg", "w") as cfg:
     cfg.writelines(lines)
