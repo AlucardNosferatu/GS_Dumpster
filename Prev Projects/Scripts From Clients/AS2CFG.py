bc97399ff0ad089a1f2b200d8c1de829cabca942
Ebenezer Scrooge <1641367382@qq.com>
2021-01-11 16:02:43 +0800

    Update AS2CFG.py

--- Prev Projects/Scripts From Clients/AS2CFG.py
+++ Prev Projects/Scripts From Clients/AS2CFG.py
@@ -1,22 +1,27 @@
-
-
-lines=[]
-with open("UREnhanced.as", "r") as asp:  # 打开文件
-    id=asp.name.replace('.as','')
-    send_start = 'dick ' + '"upload_start ' + id + '"' + '\n'
-    lines.append(send_start)
-    for i in range(2):
-        lines.append('wait\n')
-    while True:
-        code_sentence=asp.readline()
-        if not code_sentence:
-            break
-        else:
-            send_content = 'dick ' + '"upload_send_line ' + id + ' ' + code_sentence.replace('\n', '').replace('"', '$QUOTE$') + '"' + '\n'
-            lines.append(send_content)
-            for i in range(2):
-                lines.append('wait\n')
-    send_stop = 'dick ' + '"upload_stop ' + id + '"' + '\n'
-    lines.append(send_stop)
-with open("upload_asp.cfg", "w") as cfg:
-    cfg.writelines(lines)
+def GetCFG(input_file="UREnhanced.as", output_file="upload_asp.cfg"):
+    lines = []
+    with open(input_file, "r") as asp:  # 打开文件
+        id = asp.name.replace('.as', '')
+        send_start = 'dick ' + '"upload_start ' + id + '"' + '\n'
+        lines.append(send_start)
+        for i in range(2):
+            lines.append('wait\n')
+        while True:
+            code_sentence = asp.readline()
+            if not code_sentence:
+                break
+            else:
+                code_sentence = code_sentence.replace('\n', '')
+                code_sentence = code_sentence.replace('"', '$QUOTE$')
+                send_content = 'dick '
+                send_content += '"upload_send_line '
+                send_content += (id + ' ')
+                send_content += code_sentence
+                send_content += ('"' + '\n')
+                lines.append(send_content)
+                for i in range(2):
+                    lines.append('wait\n')
+        send_stop = 'dick ' + '"upload_stop ' + id + '"' + '\n'
+        lines.append(send_stop)
+    with open(output_file, "w") as cfg:
+        cfg.writelines(lines)
