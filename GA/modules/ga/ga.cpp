f22fdcfde7b757249daf1f731157f784742abb3c
Ebenezer Scrooge <1641367382@qq.com>
2021-01-26 11:15:45 +0800

    Update

--- GA/modules/ga/ga.cpp
+++ GA/modules/ga/ga.cpp
@@ -10,19 +10,35 @@ static cell AMX_NATIVE_CALL test_ga(AMX* amx, cell* params) {
 	gTask->Eva();
 	gTask->Update();
 	gTask->Eva();
-	Test BestForNow = gTask->GetBest();
-	cell* a = MF_GetAmxAddr(amx, params[4]);
-	*a = int(std::round(BestForNow.a));
-	cell b = int(std::round(BestForNow.b));
-	return b;
+	const Test BestForNow = gTask->GetBest();
+	cell* a = MF_GetAmxAddr(amx, params[1]);
+	cell* b = MF_GetAmxAddr(amx, params[2]);
+	cell* c = MF_GetAmxAddr(amx, params[3]);
+	cell* d = MF_GetAmxAddr(amx, params[4]);
+	*a = static_cast<int>(std::round(BestForNow.a));
+	*b = static_cast<int>(std::round(BestForNow.b));
+	*c = static_cast<int>(std::round(BestForNow.c));
+	*d = static_cast<int>(std::round(BestForNow.d));
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL init_task(AMX* amx, cell* params) {
+
+	const int population = params[1];
+	const double x = static_cast<double>(amx_ctof(params[2]));
+	const double y = static_cast<double>(amx_ctof(params[3]));
+	const double z = static_cast<double>(amx_ctof(params[4]));
+	const double w = static_cast<double>(amx_ctof(params[5]));
+	gTask = new GATask(population, x, y, z, w);
+	return 1;
 }
 
 AMX_NATIVE_INFO natives[] = {
 	{ "test_ga", test_ga },
+	{ "init_task", init_task },
 	{ NULL, NULL }
 };
 
 void OnAmxxAttach() {
-	gTask = new GATask(200, 7.15, 2.22, 8.4, 6.07);
 	MF_AddNatives(natives);
 }
--- GA/plugins/ga.sma
+++ GA/plugins/ga.sma
@@ -19,12 +19,16 @@ public plugin_init()
 
 public test_ga_loop()
 {
+	init_task(200,20.0,29.0,12.0,24.0);
 	set_task(1.0, "test_ga_once", .flags="b")
 }
 
 public test_ga_once()
 {
 	new ret1;
-	new ret2=test_ga("LostXmas",2029,1224,ret1);
-	server_print("Ret:%d %d",ret1,ret2);
+	new ret2;
+	new ret3;
+	new ret4;
+	test_ga(ret1,ret2,ret3,ret4);
+	server_print("Ret:%d %d %d %d",ret1,ret2,ret3,ret4);
 }
