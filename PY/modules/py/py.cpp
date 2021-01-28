602838de5bfaccd4480b960640070daf587207da
Ebenezer Scrooge <1641367382@qq.com>
2021-01-28 14:32:26 +0800

    Fuck PY, Fuck CPP

---
+++ PY/modules/py/py.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <math.h>
+#include "amxxmodule.h"
+
+#include <Python.h>
+
+using namespace std;
+
+static cell AMX_NATIVE_CALL init_py(AMX* amx, cell* params)
+{
+	Py_Initialize();
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL get_individual(AMX* amx, cell* params)
+{
+
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL eval_py(AMX* amx, cell* params)
+{
+
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL update_gen(AMX* amx, cell* params)
+{
+	return 1;
+}
+
+static cell AMX_NATIVE_CALL exit_py(AMX* amx, cell* params)
+{
+	return 1;
+}
+
+AMX_NATIVE_INFO natives[] = {
+	{ "init_py", init_py },
+	{ "get_individual", get_individual },
+	{ "eval_py", eval_py },
+	{ "update_gen", update_gen },
+	{ "exit_py", exit_py },
+	{ NULL, NULL }
+};
+
+void OnAmxxAttach() {
+	MF_AddNatives(natives);
+}
---
+++ PY/plugins/py.sma
@@ -0,0 +1,27 @@
+#include <amxmodx>
+#include <py>
+
+/*
+* 
+* Requests & Bugs:
+*	DivinityX@live.se
+*   DivinityX.no-ip.org/forum
+* 
+*/
+
+//new InCMD;
+
+public plugin_init()
+{
+	register_plugin("Sample", "1.0", "Scrooge2029");
+	register_concmd("python","python_util")
+}
+
+
+
+public python_util()
+{
+	init_py();
+}
+
+
